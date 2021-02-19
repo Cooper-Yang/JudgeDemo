9dd8a673e6f8e787b327815c6e09a3281b0127e2
Ayesen <wx566@nyu.edu>
2021-02-19 04:10:07 -0500

    DeskTopStuff

---
+++ Assets/Ivory'sFolder/Scripts/StateMachine/DeskState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskState : StateBase
+{
+
+    public override void EnterState(DeskTopCtrl DeskTop)
+    {
+        //Zoom out to desk top
+        //Disable Computer Stuf
+        //Enable DeskTop stuff
+        DeskTop.StartCoroutine(DeskTop.LerpCamPos(DeskTop.DeskTopCamPos));
+        DeskTop.Monitor.gameObject.SetActive(true);
+    }
+
+    public override void StayOnState(DeskTopCtrl DeskTop)
+    {
+        //interaction with objects
+        //run background animation
+    }
+
+    public override void LeaveState(DeskTopCtrl DeskTop)
+    {
+
+    }
+
+}
