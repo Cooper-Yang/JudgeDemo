53638a14450853d8937d4fc543e0c1ac15b18bb9
Cooper Yang <ty1037@nyu.edu>
2021-03-04 15:48:57 -0500

    Everything looks good

---
+++ Assets/CooperFolder/Scipts/CrimialEvidence.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrimialEvidence : MonoBehaviour
+{
+    public List<string> theEvidenceComparedTo;
+    public List<string> theEvidenceContained;
+}
---
+++ Assets/CooperFolder/Scipts/EvidenceManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EvidenceManager : MonoBehaviour
+{
+    public List<GameObject> suspectList;
+
+    private Dictionary<string, List<string>> theEvidence; //evidence we are comparing to, the index should be the name of criminal, the evidence list is now a string list, but it can be changed
+    
+    public TMP_Dropdown dropDown;
+    
+    private static EvidenceManager _instance;
+
+    public static EvidenceManager Instance => _instance;
+    
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+        
+    }
+
+    private void Start()
+    {
+        theEvidence = new Dictionary<string, List<string>>();
+        foreach (GameObject i in suspectList)
+        {
+            theEvidence.Add(i.name,i.GetComponent<CrimialEvidence>().theEvidenceComparedTo);
+        }
+    }
+
+    public void HandleInputData()
+    {
+        int k = dropDown.value;
+        foreach (GameObject i in suspectList)
+        {
+            i.SetActive(false);
+        }
+        suspectList[k].SetActive(true);
+    }
+
+    public void CompareEvidence()
+    {
+        int k = dropDown.value;
+        List<string> thisEvidence = new List<string>();
+        bool ifCorrect = false;
+        if (theEvidence.TryGetValue(suspectList[k].name,out thisEvidence))
+        {
+            foreach (string s in thisEvidence)
+            {
+                
+            }
+        }
+        
+    }
+}
