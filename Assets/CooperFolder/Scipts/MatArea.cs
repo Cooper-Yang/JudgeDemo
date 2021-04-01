49d6b469518c0f7b86ff0429fc103d3f33784798
Cooper Yang <ty1037@nyu.edu>
2021-04-01 13:56:06 -0400

    Not finished

---
+++ Assets/CooperFolder/Scipts/MatArea.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatArea : MonoBehaviour
+{
+    public List<RectTransform> inArea = new List<RectTransform>();
+    public GameObject SumbitArea;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindMaterialInArea();
+        SumbitArea = GameObject.Find("Submit Area");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FindMaterialInArea()
+    {
+        inArea.Clear();
+ 
+        for (int i = 0; i < transform.childCount; i++) 
+        {
+            if(transform.GetChild(i).gameObject.CompareTag("Material"))
+            {
+                inArea.Add(transform.GetChild(i).gameObject.GetComponent<RectTransform>());
+            }
+                 
+        }
+    }
+
+    public void RectOverlaps()
+    {
+        
+        foreach (RectTransform rectTransform in SumbitArea.GetComponent<SubmitArea>().inArea)
+        {
+            Rect rect1 = new Rect(rectTransform.localPosition.x, rectTransform.localPosition.y, rectTransform.rect.width, rectTransform.rect.height);
+            Rect rect2 = new Rect(GetComponent<RectTransform>().localPosition.x, GetComponent<RectTransform>().localPosition.y, GetComponent<RectTransform>().rect.width, GetComponent<RectTransform>().rect.height);
+            Debug.Log("called");
+
+            if (rect1.Overlaps(rect2))
+            {
+                Debug.Log("overlap");
+
+                rectTransform.transform.SetParent(this.transform);
+            }
+        }
+        FindMaterialInArea();
+    }
+}
---
+++ Assets/CooperFolder/Scipts/SubmitArea.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmitArea : MonoBehaviour
+{
+    public List<RectTransform> inArea = new List<RectTransform>();
+    public GameObject MaterialArea;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindMaterialInArea();
+        MaterialArea = GameObject.Find("Material Area");
+    }
+
+    // Update is called once per frame
+
+    void FindMaterialInArea()
+    {
+        inArea.Clear();
+ 
+        for (int i = 0; i < transform.childCount; i++) 
+        {
+            if(transform.GetChild(i).gameObject.CompareTag("Material"))
+            {
+                inArea.Add(transform.GetChild(i).gameObject.GetComponent<RectTransform>());
+            }
+                 
+        }
+    }
+    
+    public void RectOverlaps()
+    {
+
+        foreach (RectTransform rectTransform in MaterialArea.GetComponent<MatArea>().inArea)
+        {
+            Rect rect1 = new Rect(rectTransform.localPosition.x, rectTransform.localPosition.y, rectTransform.rect.width, rectTransform.rect.height);
+            Rect rect2 = new Rect(GetComponent<RectTransform>().localPosition.x, GetComponent<RectTransform>().localPosition.y, GetComponent<RectTransform>().rect.width, GetComponent<RectTransform>().rect.height);
+            
+            
+            if (rect1.Overlaps(rect2))
+            {
+                
+                rectTransform.transform.SetParent(transform);
+            }
+        }
+        FindMaterialInArea();
+    }
+    
+}
