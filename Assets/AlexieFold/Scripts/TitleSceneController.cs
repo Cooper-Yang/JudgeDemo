2fd8ae5afdc72aa473db1bbbc1632c6e92524f46
TakayaEnomoto <49789142+TakayaEnomoto@users.noreply.github.com>
2021-04-28 07:40:41 +0800

    Start scene finished. Cooper's Sound Man has been changed into dont destroy on load. If any conflict appear, don't use my version.

--- Assets/AlexieFold/Scripts/TitleSceneController.cs
+++ Assets/AlexieFold/Scripts/TitleSceneController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleSceneController : MonoBehaviour
 {
     public TMP_InputField password;
     public TextMeshProUGUI warning;
     public TextMeshProUGUI hint;
+    public TextMeshProUGUI title;
+    public Image blackOutSqure;
+    public Camera mainCamera;
     private int wrongTries;
+    private float fadeOutTimer;
+    private bool playTitle;
     // Start is called before the first frame update
     void Start()
     {
         wrongTries = 0;
+        fadeOutTimer = 0;
+        playTitle = false;
     }
 
     // Update is called once per frame
@@ -21,18 +29,35 @@ public class TitleSceneController : MonoBehaviour
     {
         if (wrongTries >= 3)
             hint.text = "Hint: Birthday MMDD";
+        if (playTitle)
+        {
+            fadeOutTimer += 0.08f * Time.deltaTime;
+            AmbienceFadeOut();
+        }
     }
 
     public void StartButton()
     {
         if (password.text == "1104")
+        {
+            playTitle = true;
+            StopAllCoroutines();
+            StartCoroutine(PlayTitle());
             Debug.Log("Password correct");
+        }
         else
         {
             StopAllCoroutines();
             StartCoroutine(StartWarning());
-            wrongTries += 1;
+            if(password.text != "")
+                wrongTries += 1;
         }
+    }
+    public void AmbienceFadeOut()
+    {
+        GameObject ab = GameObject.Find("Ambience");
+        ab.GetComponent<AudioLowPassFilter>().cutoffFrequency = Mathf.Lerp(4000f, 22000f, fadeOutTimer);
+        ab.GetComponent<AudioSource>().volume = Mathf.Lerp(0.5f, 0.9f, fadeOutTimer);
     }
 
     IEnumerator StartWarning()
@@ -43,5 +68,58 @@ public class TitleSceneController : MonoBehaviour
             warning.text = "Password incorrect";
         yield return new WaitForSeconds(2f);
         warning.text = "";
+    }
+
+    IEnumerator PlayTitle(float fadeSpeed = 0.2f)
+    {
+        Color objectColor = blackOutSqure.GetComponent<Image>().color;
+        Color titleColor = title.color;
+        float fadeAmount;
+
+        if (playTitle)
+        {
+            while(blackOutSqure.GetComponent<Image>().color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSqure.GetComponent<Image>().color = objectColor;
+
+                mainCamera.orthographicSize += 3 * Time.deltaTime;
+                yield return null;
+            }
+
+            if (blackOutSqure.GetComponent<Image>().color.a >= 1)
+            {
+                while (title.color.a < 1)
+                {
+                    fadeAmount = titleColor.a + (fadeSpeed * Time.deltaTime);
+
+                    titleColor = new Color(titleColor.r, titleColor.g, titleColor.b, fadeAmount);
+                    title.color = titleColor;
+                    yield return null;
+                }
+            }
+
+            yield return new WaitForSeconds(2f);
+
+            if(title.color.a >= 1)
+            {
+                while(title.color.a > 0)
+                {
+                    fadeAmount = titleColor.a - (fadeSpeed * Time.deltaTime);
+
+                    titleColor = new Color(titleColor.r, titleColor.g, titleColor.b, fadeAmount);
+                    title.color = titleColor;
+
+                    GameObject ab = GameObject.Find("Ambience");
+                    ab.GetComponent<AudioSource>().volume = Mathf.Lerp(0.5f, 0.9f, title.color.a);
+                    yield return null;
+                }
+            }
+
+            yield return new WaitForSeconds(0.5f);
+            SceneManager.LoadScene(0);
+        }
     }
 }
