c15b5c90a44ad7471d8b16fab59c202181aa20eb
TakayaEnomoto <49789142+TakayaEnomoto@users.noreply.github.com>
2021-04-28 05:53:46 +0800

    Title scene structure done.

---
+++ Assets/AlexieFold/Scripts/TitleSceneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TitleSceneController : MonoBehaviour
+{
+    public TMP_InputField password;
+    public TextMeshProUGUI warning;
+    public TextMeshProUGUI hint;
+    private int wrongTries;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wrongTries = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wrongTries >= 3)
+            hint.text = "Hint: Birthday MMDD";
+    }
+
+    public void StartButton()
+    {
+        if (password.text == "1104")
+            Debug.Log("Password correct");
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(StartWarning());
+            wrongTries += 1;
+        }
+    }
+
+    IEnumerator StartWarning()
+    {
+        if (password.text == "")
+            warning.text = "Please enter password";
+        else
+            warning.text = "Password incorrect";
+        yield return new WaitForSeconds(2f);
+        warning.text = "";
+    }
+}
