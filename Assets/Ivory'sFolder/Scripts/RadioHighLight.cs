322226900ec70bfb6c1198f24e83258f7791f2bc
Cooper Yang <ty1037@nyu.edu>
2021-05-07 05:40:56 +0800

    Radio Worked

---
+++ Assets/Ivory'sFolder/Scripts/RadioHighLight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioHighLight : MonoBehaviour
+{
+    //public DeskTopCtrl deskcon;
+    private Color StartColor = Color.white;
+    private Color HightlightColor = Color.yellow;
+    public GameObject radioMan;
+
+    void OnMouseEnter()
+    {
+        CursorManager.Instance.ChangeToClick();
+        this.GetComponent<Renderer>().material.color = HightlightColor;
+    }
+    void OnMouseExit()
+    {
+        CursorManager.Instance.ChangeToDefault();
+        this.GetComponent<Renderer>().material.color = StartColor;
+    }
+
+    void OnMouseDown()
+    {
+        radioMan.GetComponent<RadioControl>().PauseOrResume();
+    }
+}
